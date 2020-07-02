309bef6c409c0020b3cdd81b67f436058b6bea86
Kuily <kuily@Niroshans-MacBook-Pro.local>
2020-07-02 15:27:55 +0100

    adding code for toggle buttons

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const dailyquote = document.getElementById('quote');
 const quotebtn = document.getElementById('btn');
-
-
+const toggle = document.getElementById('toggle');
+const join = document.getElementById('join')
 quotebtn.addEventListener('click', function getquote(){
     fetch(`https://quote-garden.herokuapp.com/api/v2/quotes/random`)
     .then(res => res.json())
@@ -10,4 +10,12 @@ quotebtn.addEventListener('click', function getquote(){
         console.log(data);
         
     })
+})
+
+toggle.addEventListener('click', function(){
+    document.body.classList.toggle('appear');
+})
+
+join.addEventListener('click', function(){
+    document.body.classList.toggle('appear-form');
 })
