316f13bd1cdb6bc94cf34ed12054d2239673308a
Kuily <kuily@Niroshans-MacBook-Pro.local>
2020-07-07 22:46:26 +0100

    fixing projects css

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+/////////////////////////////////////////////////////////////////////
+// dom manipulation ////////////////////////////////////////////////
+
 const dailyquote = document.getElementById('quote');
-const quotebtn = document.getElementById('btn');
+const quotebtn = document.getElementById('quote-btn');
 const toggle = document.getElementById('toggle');
-const join = document.getElementById('join')
+const formBtn = document.getElementById('form-btn');
+const containerForm = document.getElementById('container-form')
+
 quotebtn.addEventListener('click', function getquote(){
     fetch(`https://quote-garden.herokuapp.com/api/v2/quotes/random`)
     .then(res => res.json())
@@ -16,6 +21,19 @@ toggle.addEventListener('click', function(){
     document.body.classList.toggle('appear');
 })
 
-join.addEventListener('click', function(){
-    document.body.classList.toggle('appear-form');
+formBtn.addEventListener('click', function(){
+    containerForm.classList.toggle('open-form');
 })
+
+//////////////////////////////////////////////////////////////
+// form validation////////////////////////////////////////////
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+if(name.value >=3){
+    name.classList.add('name-green')
+}else{
+    name.classList.add('name-red')
+}
