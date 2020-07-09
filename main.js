a6d466b058c743548bcfc778cef11e14cf2fe785
Kuily <kuily@Niroshans-MacBook-Pro.local>
2020-07-09 16:12:48 +0100

    3rd changes to css and javascript

--- main.js
+++ main.js
@@ -28,10 +28,10 @@ formBtn.addEventListener('click', function(){
 //////////////////////////////////////////////////////////////
 // form validation////////////////////////////////////////////
 const name = document.getElementById('name');
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
-const password2 = document.getElementById('password2').value;
-const number = document.getElementById('number').value;
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const number = document.getElementById('number');
 const form = document.getElementById('form');
 
 
@@ -41,9 +41,11 @@ const form = document.getElementById('form');
 form.addEventListener('submit', function(e){
     e.preventDefault();
     console.log('click');
-    
-    // checkName(name, 2, 10);
+    checkNumber(number);
+    checkName(name);
     checkEmail(email);
+    checkPassword(password);
+    checkPassword2(password, password2);
     
     
 })
@@ -54,17 +56,55 @@ function errormsg(input, message){
     const small= inputArea.querySelector('small');
     small.innerText = message
 }
-function correctmsg(input, message){
+function correctmsg(input){
     inputArea=input.parentElement
     inputArea.className = 'inputArea correct small'
-    small.innerText = message;
 }
 
 function checkEmail(input){
     let cc = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
     if(cc.test(input.value)){
-        correctmsg(input, 'correct')
+        correctmsg(input)
     }else{
         errormsg(input, 'invalid email')
     }
 }
+
+function checkName(input){
+    if(input.value === ''){
+        errormsg(input, 'name required')
+    }else if(input.value.length <= 2){
+        errormsg(input, 'minimum 2 characters required')
+    }else{
+        correctmsg(input)
+    }
+}
+function checkNumber(input){
+    console.log("typeof input is: ", typeof input.value);
+    console.log(isNaN(input));
+    if(typeof input === number){
+        correctmsg(input);
+    }else{
+        errormsg(input, 'invalid number format')
+    }
+}
+function checkPassword(input){
+    if(input.value === ''){
+        errormsg(input, 'password is required')
+        }else if(input.value.length<4){
+            errormsg(input, 'minimum 4 characters is required')
+        }else{
+            correctmsg(input)
+        }
+}
+function checkPassword2(input1, input2){
+    if(input1.value !== input2.value){
+        errormsg(input2, 'password do not match')
+        
+    }else if(input2.value===''){
+        errormsg(input2, 'enter password again')
+    }else{
+        correctmsg(input2)
+    }
+}
+
