4d99766618d98d90a18090b118b1ddee196ed258
Kuily <kuily@Niroshans-MacBook-Pro.local>
2020-07-23 09:54:49 +0100

    modifing es5 functions  to  arrow functions

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const dailyquote = document.getElementById('quote');
 const quotebtn = document.getElementById('quote-btn');
 const toggle = document.getElementById('toggle');
 
-quotebtn.addEventListener('click', function getquote(){
+quotebtn.addEventListener('click', () => {
     fetch(`https://quote-garden.herokuapp.com/api/v2/quotes/random`)
     .then(res => res.json())
     .then(data => {
@@ -13,6 +13,6 @@ quotebtn.addEventListener('click', function getquote(){
     })
 })
 
-toggle.addEventListener('click', function(){
+toggle.addEventListener('click', () => {
     document.body.classList.toggle('appear');
 })
