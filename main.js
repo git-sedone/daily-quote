310e069c81cac94a988f31cd8e25b98e9fb34970
Kuily <kuily@Niroshans-MacBook-Pro.local>
2023-01-01 19:26:06 +0000

    changed api

--- main.js
+++ main.js
@@ -1,30 +1,23 @@
-
 const getBtn = document.getElementById('get-btn');
-getBtn.addEventListener('click', ()=>{
-    fetch(`https://quote-garden.herokuapp.com/api/v3/quotes/random`)
-    .then(res => res.json())
-    .then(res => {
-        // destructed way
-        const [{ _id, quoteText}] = res.data;
-        quote.innerHTML = `${quoteText}`
+getBtn.addEventListener('click', () => {
+  fetch('https://dummyjson.com/quotes/random')
+    .then((res) => res.json())
+    .then((res) => {
+      quote.innerHTML = `${res.quote}`;
     })
-    .catch(error => {
-    
-        throw error;
-    })
-})
+    .catch((error) => {
+      throw error;
+    });
+});
 
 let toggleButton = document.getElementById('toggle-btn');
 let mobileNav = document.getElementById('mobile-nav');
 let closeNav = document.getElementById('close');
 
-toggleButton.addEventListener('click', function(){
-    
-    mobileNav.style.display='block'
+toggleButton.addEventListener('click', function () {
+  mobileNav.style.display = 'block';
 });
 
-closeNav.addEventListener('click', function(){
-    
-    mobileNav.style.display='none'
-    
-})
+closeNav.addEventListener('click', function () {
+  mobileNav.style.display = 'none';
+});
